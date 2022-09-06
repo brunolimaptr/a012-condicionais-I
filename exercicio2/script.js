3fa52c5c72ca9c53b5f3024caafef7405792a012
brunolimaptr <brunolima.ptr@gmail.com>
2022-09-06 15:56:56 -0300

    projeto

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,33 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
 
-if(){
+let idade = prompt("Qual a sua idade?")
+let terminouEM = prompt("Terminou o ensino medio?")
+let CursandoalgumaFaculdade = confirm("Está cursando alguma faculdade?")
 
-}else{
-	
+
+if (idade >= 18){
+    console.log("É maior de idade!")
 }
+
+else if (idade < 18){
+    console.log("É menor de idade!")
+}
+
+else{console.log("Dado invalido!")}
+
+if (terminouEM === "sim"){
+    console.log("Terminou o ensino médio!")
+    
+}
+
+else if(terminouEM === "nao"){
+    console.log("Não terminou o ensino médio!")
+    
+}
+
+else{console.log("Dado invalido!")}
+
+if (!CursandoalgumaFaculdade){
+    console.log("Não está na faculdade!")
+}
+
+else {console.log("Está na faculdade!")}
